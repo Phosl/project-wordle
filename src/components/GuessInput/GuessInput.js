b742e196e2238138cb744e3fef6b7180c7018d04
fdg <filippodegennaro@hotmail.it>
2024-10-26 11:36:41 +0200

    Add files via upload

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function GuessInput({handleSubmitGuess}) {
+  const [tentativeGuess, setTentativeGuess] = React.useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    handleSubmitGuess(tentativeGuess)
+    setTentativeGuess('')
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        value={tentativeGuess}
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase()
+          setTentativeGuess(nextGuess)
+        }}
+      />
+    </form>
+  )
+}
+
+export default GuessInput
